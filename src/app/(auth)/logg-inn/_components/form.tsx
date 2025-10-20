6a6020cc131be2e292d626b39b065499aa2fd59a
Josefine Huffman <Josefinehuffman@icloud.com>
2025-10-20 19:11:18 +0200

    Fixed with

---
+++ src/app/(auth)/logg-inn/_components/form.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import type z from "zod";
+import { login } from "~/actions";
+import FormInput from "~/components/form/input";
+import { Button } from "~/components/ui/button";
+import { Card, CardContent } from "~/components/ui/card";
+import { Form } from "~/components/ui/form";
+import { authClient } from "~/lib/auth-client";
+import { cn } from "~/lib/utils";
+import { SignInInputSchema } from "~/schemas";
+
+export function LoginForm({
+	className,
+	...props
+}: React.ComponentProps<"div">) {
+	const [isPending, startTransition] = useTransition();
+	const router = useRouter();
+
+	const form = useForm<z.infer<typeof SignInInputSchema>>({
+		resolver: zodResolver(SignInInputSchema),
+		defaultValues: {
+			username: "",
+			password: "",
+		},
+	});
+
+	const onSubmit = async (values: z.infer<typeof SignInInputSchema>) => {
+		startTransition(async () => {
+			try {
+				const formData = new FormData();
+				formData.append("username", values.username);
+				formData.append("password", values.password);
+				const loginResponse = await login(formData);
+				if (loginResponse.formError || !loginResponse.userData) {
+					toast.error(loginResponse.formError);
+					form.reset({
+						password: "",
+						username: "",
+					});
+					return;
+				}
+
+				if (loginResponse.userData.isNewUser) {
+					await authClient.signUp.email({
+						email: loginResponse.userData.email,
+						password: values.password,
+						name: loginResponse.userData.fullName,
+						username: loginResponse.userData.username,
+						isAdmin: false,
+					});
+				} else {
+					await authClient.signIn.email({
+						password: values.password,
+						email: loginResponse.userData.email,
+					});
+				}
+
+				router.replace("/min-oversikt");
+				router.refresh();
+			} catch {
+				toast.error(
+					"Noe gikk galt under innloggingen. Vennligst prøv igjen senere.",
+				);
+			}
+		});
+	};
+
+	return (
+		<div className={cn("flex flex-col gap-6", className)} {...props}>
+			<Card className="overflow-hidden p-0">
+				<CardContent className="grid p-0">
+					<Form {...form}>
+						<form className="p-6 md:p-8" onSubmit={form.handleSubmit(onSubmit)}>
+							<div className="flex flex-col gap-6">
+								<div className="flex flex-col items-center text-center">
+									<h1 className="font-bold text-2xl">Velkommen tilbake</h1>
+									<p className="text-balance text-muted-foreground">
+										Logg inn med din TIHLDE bruker
+									</p>
+								</div>
+								<FormInput
+									form={form}
+									name="username"
+									required
+									label="Brukernavn"
+								/>
+
+								<FormInput
+									form={form}
+									name="password"
+									type="password"
+									required
+									label="Passord"
+								/>
+
+								<Button type="submit" className="w-full" disabled={isPending}>
+									{isPending ? (
+										<Loader2 className="animate-spin" />
+									) : (
+										<span>Logg inn</span>
+									)}
+								</Button>
+							</div>
+						</form>
+					</Form>
+					<div className="relative hidden items-center justify-center bg-card md:flex">
+						{/* <Image
+							src="/images/signin.png"
+							alt="Logg inn"
+							width={300}
+							height={200}
+						/> */}
+					</div>
+				</CardContent>
+			</Card>
+		</div>
+	);
+}

dce28ebcefb53b27a40af78c34078b41e81b1a36
André Skjellevik <42360069+askjellevik@users.noreply.github.com>
2026-04-19 15:24:26 +0200

    remove dead toaster code replaced by new confirmation site

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
-import { Suspense } from "react";
 
 import BottomBar from "~/components/navigation/bottom-bar";
 import Footer from "~/components/navigation/footer";
 import Navbar from "~/components/navigation/top-bar";
-import { RsvpToast } from "./_components/rsvp-toast";
 
 export const metadata: Metadata = {
 	title: "Idrett | TIHLDE",
@@ -18,9 +16,6 @@ export default async function RootLayout({
 }>) {
 	return (
 		<div className="relative min-h-dvh">
-			<Suspense>
-				<RsvpToast />
-			</Suspense>
 			<Navbar />
 			<main className="w-full">{children}</main>
 			<BottomBar />
